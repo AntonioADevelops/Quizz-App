a41ced1e0cf45eebf210860e70ec33932b6c17fb
AntonioADevelops <ayalarantonio16@gmail.com>
2022-01-28 09:56:36 -0800

    good

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
     const Questions = ["Question1", "Question2", "Question3", "Question4", "Question5"];
     const Correct = ["Correct1", "Correct2", "Correct3", "Correct4", "Correct5"]
     const Wrong = ["Wrong1", "Wrong2", "Wrong3", "Wrong4", "Wrong5", "Wrong6", "Wrong7", "Wrong8", "Wrong9", "Wrong10"]
-    const Correspond = ["1", "2", "3", "4", "5"]
+    const Correspond = ["1", "2", "3", "4", "5"] A
 });
