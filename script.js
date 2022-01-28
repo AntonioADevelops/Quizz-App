410379130b59858bb927d54324bc4b74b23a89a5
AntonioADevelops <ayalarantonio16@gmail.com>
2022-01-28 09:45:32 -0800

    jquery

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var items = {
+        "Question 1":"Answer 1",
+        "Question 2":"Answer 2",
+        "Questions 3":"Answer 3",
+    };
+    $("#btn1").text("Hello world!");
+});
